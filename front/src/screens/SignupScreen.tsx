a0d91dc6bd0e7f6cf256cd3046cbe4467e383097
dnlbabkin <danilbabkin57@gmail.com>
2024-03-19 16:33:04 +0300

    finish transaction screen

--- front/src/screens/SignupScreen.tsx
+++ front/src/screens/SignupScreen.tsx
@@ -1,5 +1,5 @@
 import React, { SyntheticEvent, useState } from "react";
-import { Form, Button, Modal } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 import FormContainer from "../components/FormContainer";
 import { useNavigate } from "react-router-dom";
 import ModalWindow from "../components/ModalWindow";
